ce2eab60946d45d6ba4bc4bc9aa3f963e3a1d8ef
Dale Salter <djsalter93@hotamil.com>
2014-12-10 20:17:38 +1100

    ADDED: Tabbing on input forms

--- client/app/post/post-blog/post-blog-controller.js
+++ client/app/post/post-blog/post-blog-controller.js
@@ -44,6 +44,34 @@
         }
       };
 
+      setTimeout(function(){
+        enableTab('blog-input');
+      }, 1000);
+
       // Executes to initally set up the data
       $scope.reset();
     });
+
+    // Changes the tab from moving between forms to an actual tab character
+    function enableTab(id) {
+      var el = document.getElementById(id);
+      el.onkeydown = function(e) {
+        if (e.keyCode === 9) { // tab was pressed
+
+          // get caret position/selection
+          var val = this.value,
+          start = this.selectionStart,
+          end = this.selectionEnd;
+
+          // set textarea value to: text before caret + tab + text after caret
+          this.value = val.substring(0, start) + '\t' + val.substring(end);
+
+          // put caret at right position again
+          this.selectionStart = this.selectionEnd = start + 1;
+
+          // prevent the focus lose
+          return false;
+
+        }
+      };
+    }
